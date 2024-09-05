4719507ef680666e74d47db09b08a30cf461dd92
al6894 <115506396+al6894@users.noreply.github.com>
2024-09-05 16:11:01 -0400

    added fade-in and fade-out

--- static/index.js
+++ static/index.js
@@ -6,12 +6,15 @@ const beginJourneyLink = document.querySelector('.begin-journey');
 beginJourneyLink.addEventListener('click', (e)=> {
     e.preventDefault();
 
+    document.body.classList.add('fade-out');
     let value = e.clientX;
 
     left_trees.style.transform = `translateX(${value * -1.9}px)`; // Adjust to smaller value for visible effect
     right_trees.style.transform = `translateX(${value * 1.3}px)`; 
 
+    mountain.style.transform = 'scale(10)'
+
     setTimeout(()=> {
         window.location.href = "home.html";
-    }, 700);
+    }, 1000);
 });
